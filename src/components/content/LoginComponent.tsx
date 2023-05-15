e506918abefe39dc11e7028ed5b1acd0f9860ccf
valoon3 <valoon2@naver.com>
2023-05-15 18:23:50 +0900

    <feat> docker aws 배포 주소 변경

--- src/components/content/LoginComponent.tsx
+++ src/components/content/LoginComponent.tsx
@@ -1,18 +1,32 @@
 import AppLayout from '@/components/layout/AppLayout';
+import axios from 'axios';
 
 const LoginComponent = () => {
+
+  const userInfo = { userEmail: 'asdf', password: '1234', name: '나병호' };
+
+  const onClick = () => {
+    console.log('로그인');
+    axios.post('user/login', userInfo)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
       <AppLayout>
         <div>
-          <input type="text" />
-          <input type="password" />
-          <button>로그인</button>
+          <input type='text' />
+          <input type='password' />
+          <button onClick={onClick}>로그인</button>
         </div>
       </AppLayout>
     </>
   );
-}
+};
 
 export default LoginComponent;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 // import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import axios from 'axios';
+import * as process from 'process';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  if(process.env.NODE_ENV === 'development') {
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_DEV;
+  } else if(process.env.NODE_ENV === 'production') {
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_PRO;
+  } else if(process.env.NODE_ENV === 'test') {
+
+  }
+
+
+
   return <Component {...pageProps} />
 }
