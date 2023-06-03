2940e0d6e795ff220df12cf7fb05aa458fbd6c5a
valoon3 <valoon2@naver.com>
2023-06-03 12:49:15 +0900

    <feat> tailwind inputGroup 컴포넌트 분리
    1.

---
+++ src/components/InputGroup.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface InputGroupProps {
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+const InputGroup:React.FC<InputGroupProps> = ({
+  className = "mb-2",
+  type= "text",
+  placeholder = "",
+  value,
+  error,
+  setValue
+}) => {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        style={{ minWidth: 300 }}
+        className="w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white"
+        placeholder={placeholder}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-500">{error}</small>
+    </div>
+  )
+}
+
+export default InputGroup;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from 'next/link';
+import InputGroup from '@/components/InputGroup';
 
 const register = () => {
   return (
@@ -8,11 +10,17 @@ const register = () => {
           <div className="w-10/12 mx-auto md:w-96">
             <h1 className="mb-2 text-lg font-medium">회원가입</h1>
             <form>
-
+              <InputGroup value={"asf"} error={"asdf"} setValue={(name) => name} />
+              <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+                회원가입
+              </button>
+              <small>
+                이미 가입하셨나요?
+                <Link href="/login" className="ml-1 text-blue-500 uppercase">
+                  로그인
+                </Link>
+              </small>
             </form>
-            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원가입
-            </button>
           </div>
         </div>
       </div>
