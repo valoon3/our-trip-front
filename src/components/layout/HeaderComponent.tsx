94c0b2f76d2b82f5dbc128bcba6332ccd57495c0
valoon3 <valoon2@naver.com>
2023-06-21 17:39:07 +0900

    <fix> eslint 버젼 호환 문제 해결

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -5,42 +5,9 @@ import Link from 'next/link';
 import styled from '@/styles/header.module.scss';
 import AppLayout from '@/components/layout/AppLayout';
 
-const { Header } = Layout;
-type MenuItem = Required<MenuProps>['items'][number];
-
-const SIGN_UP:React.Key = 'signUp';
-const LOGIN:React.Key = 'login';
-
 const HeaderComponent = () => {
 
   const toggleLogin: boolean = false;
-
-    function getItem(
-        label: React.ReactNode,
-        key: React.Key,
-        icon?: React.ReactNode,
-        children?: MenuItem[],
-        type?: 'group',
-    ): MenuItem {
-        return {
-          label,
-          key,
-          icon,
-          children,
-          type,
-        } as MenuItem;
-    }
-
-    const items:MenuItem[] = [
-        getItem(
-          '회원가입',
-          SIGN_UP,
-        ),
-        getItem(
-          <Link href="/" >a</Link>,
-          LOGIN,
-        ),
-    ];
 
     return (
       <header className={styled.header}>
