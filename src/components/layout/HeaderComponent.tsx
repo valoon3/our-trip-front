57f28dac2deec7c4ca19f6cf64dd98f9d0fa0797
valoon3 <valoon2@naver.com>
2023-06-22 21:33:48 +0900

    <fix> 디자인 변경

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,27 +1,23 @@
-import { Layout, Menu, Dropdown, Space } from 'antd';
-import type { MenuProps } from 'antd';
-import React from "react";
+import React from 'react';
 import Link from 'next/link';
 import styled from '@/styles/header.module.scss';
-import AppLayout from '@/components/layout/AppLayout';
 
 const HeaderComponent = () => {
-
   const toggleLogin: boolean = false;
 
-    return (
-      <header className={styled.header}>
-        <div className={styled.logo}>로고</div>
-        <div className={styled.headerRight}>
-          <div>
-            <Link href="/register" className={styled.headerFont}>회원가입</Link>
-          </div>
-          <div>
-            <Link href="/login" className={styled.headerFont}>로그인</Link>
-          </div>
-        </div>
-      </header>
-    )
-}
+  return (
+    <header className={styled.header}>
+      <div className={styled.logo}>로고</div>
+      <div className={styled.headerRight}>
+        <Link href="/register" className={styled.headerFont}>
+          회원가입
+        </Link>
+        <Link href="/login" className={styled.headerFont}>
+          로그인
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default HeaderComponent;
