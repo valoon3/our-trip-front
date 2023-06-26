a9db5e00159cc737d3cd0c951a9986024681814c
valoon3 <valoon2@naver.com>
2023-06-26 17:32:33 +0900

    <fix> global 설정 오류 변경, 로고 contentBox 벗어나는 문제 해결
    1. div margin css 설정 실수로 인한 범위 인식 오류
    2. next Head 설정 두번으로 인한 next-head-count is missing 오류 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import axios from 'axios';
 import * as process from 'process';
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  if(process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_DEV + '/api';
-  } else if(process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === 'production') {
     axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_PRO + '/api';
-  } else if(process.env.NODE_ENV === 'test') {
-
+  } else if (process.env.NODE_ENV === 'test') {
   }
 
-
-
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>Create Next App</title>
+        <Link rel="icon" href="/favicon.ico" />
+        <Link href="../styles/globals.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
