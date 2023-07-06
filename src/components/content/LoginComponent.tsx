b916bfd53650524b7d660bd3f261152e29b25183
valoon3 <valoon2@naver.com>
2023-07-06 18:50:44 +0900

    <feat> 로그인 컴포넌트 로그인, 로그아웃 기능 구현
    1. 쿠키 삭제 기능 구현 필요
    2. 로그인 상태에 따라 헤더 컴포넌트 변환

--- src/components/content/LoginComponent.tsx
+++ src/components/content/LoginComponent.tsx
@@ -1,31 +1,41 @@
-import AppLayout from '@/components/layout/AppLayout';
-import axios from 'axios';
-
 // TODO : 사용하지 않는 경우 삭제하기
+import styled from '@/styles/header.module.scss';
+import Link from 'next/link';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/app/store';
+import { logout } from '@/app/reduce/userSlice';
+
 const LoginComponent = () => {
+  const loginToggle = useSelector((state: RootState) => state.user.loginToggle);
+  const dispatch = useDispatch();
 
-  const userInfo = { userEmail: 'asdf', password: '1234', name: '나병호' };
+  const logoutAction = () => {
+    console.log('로그아웃');
+    dispatch(logout);
+  };
 
-  const onClick = () => {
-    console.log('로그인');
-    axios.post('user/login', userInfo)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const userInfoAction = () => {
+    console.log('회원정보');
   };
 
   return (
     <>
-      <AppLayout>
-        <div>
-          <input type='text' />
-          <input type='password' />
-          <button onClick={onClick}>로그인</button>
-        </div>
-      </AppLayout>
+      <div className={styled.headerRight}>
+        {!loginToggle ? (
+          <>
+            <Link href="/register">회원가입</Link>
+            <Link href="/login">로그인</Link>
+          </>
+        ) : (
+          <>
+            <button onClick={userInfoAction}>회원정보</button>
+            <Link href="/" onClick={logoutAction}>
+              로그아웃
+            </Link>
+          </>
+        )}
+      </div>
     </>
   );
 };
--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from '@/styles/header.module.scss';
+import LoginComponent from '@/components/content/LoginComponent';
 
 const HeaderComponent = () => {
   const toggleLogin: boolean = false;
@@ -12,10 +13,11 @@ const HeaderComponent = () => {
         <Link href="/" className={styled.logo}>
           로고
         </Link>
-        <div className={styled.headerRight}>
-          <Link href="/register">회원가입</Link>
-          <Link href="/login">로그인</Link>
-        </div>
+        <LoginComponent />
+        {/*<div className={styled.headerRight}>*/}
+        {/*  <Link href="/register">회원가입</Link>*/}
+        {/*  <Link href="/login">로그인</Link>*/}
+        {/*</div>*/}
       </div>
     </header>
   );
