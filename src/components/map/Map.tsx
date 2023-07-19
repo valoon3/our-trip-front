05581f032db21adebf1b0fe443276a14dc6405a4
valoon3 <valoon2@naver.com>
2023-07-19 15:55:55 +0900

    <feat> 구글 맵에서 네이버 맵으로 전환
    1. 맵만 일단 띄워둠
    2. 수정 필요

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,44 +1,49 @@
-import { useLoadScript, GoogleMap } from '@react-google-maps/api';
-import type { NextPage } from 'next';
-import { useMemo } from 'react';
+import { useEffect, useRef } from 'react';
+import Script from 'next/script';
+// import { NaverMap } from '@/types/map';
+
+type Props = {
+  // mapId?: string;
+  // initialCenter?: Coordinates;
+  // initialZoom?: number;
+  // onLoad?: (map: NaverMap) => void;
+};
 
 const MapComponent = () => {
-  const libraries = useMemo(() => ['places'], []);
-  const mapCenter = useMemo(
-    () => ({ lat: 27.672932021393862, lng: 85.31184012689732 }),
-    []
-  );
+  const mapElement = useRef(null);
 
-  const mapOptions = useMemo<google.maps.MapOptions>(
-    () => ({
-      disableDefaultUI: true,
-      clickableIcons: true,
-      scrollwheel: false,
-    }),
-    []
-  );
+  useEffect(() => {
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-    libraries: libraries as any
-  });
-
-  if (!isLoaded) {
-    return <p>Loading...</p>;
-  }
+    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    const mapOptions: naver.maps.MapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    };
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
 
   return (
-    <div id="map" style={{ height : "600px"}}>
-      <GoogleMap
-        options={mapOptions}
-        zoom={14}
-        center={mapCenter}
-        mapTypeId={google.maps.MapTypeId.ROADMAP}
-        mapContainerStyle={{ width: '800px', height: '800px' }}
-        onLoad={() => console.log('Map Component Loaded...')}
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAPS_API_KEY}`}
+        // onReady={initializeMap}
       />
-    </div>
-  )
-}
+      <div ref={mapElement} style={{ minHeight: '400px' }} />
+    </>
+  );
+};
 
 export default MapComponent;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,17 @@ import { Head, Html, Main, NextScript } from 'next/document';
 import Link from 'next/link';
 
 export default function Document() {
+  const naverMapKey =
+    'https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=' +
+    process.env.NEXT_PUBLIC_NAVER_MAPS_API_KEY;
+
   return (
     <Html lang="ko">
       <Head>
         <title>Create Next App</title>
         <Link rel="icon" href="/favicon.ico" />
         <Link href="../styles/globals.css" rel="stylesheet" />
+        <script type="text/javascript" src={naverMapKey} async></script>
       </Head>
       <body>
         <Main />
