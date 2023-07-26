cd0f169240ea603417f82f6eaf2bce64025e4358
valoon3 <valoon2@naver.com>
2023-07-26 18:30:45 +0900

    <bug> 구글 맵 다른 컴포넌트에서 구글 맵 불러오기 실패

--- src/components/layout/MapSectionComponent.tsx
+++ src/components/layout/MapSectionComponent.tsx
@@ -1,9 +1,19 @@
 import GoogleMap from '@/components/map/GoogleMap';
+import { useMemo } from 'react';
 
 const MapSectionComponent = () => {
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
   return (
     <div style={{ width: '2000px', height: '2000px' }}>
-      <GoogleMap />
+      <GoogleMap mapOptions={mapOptions} />
       {/*<KakaoMap />*/}
     </div>
   );
