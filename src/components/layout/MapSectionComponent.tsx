7547f328d0f5ec66071c61a662f376c3a5e30eea
valoon3 <valoon2@naver.com>
2023-07-29 17:34:30 +0900

    <feat> googlemaps/js-api-loader 추가
    1. 공식문서 따라가기

--- src/components/layout/MapSectionComponent.tsx
+++ src/components/layout/MapSectionComponent.tsx
@@ -1,19 +1,9 @@
 import GoogleMap from '@/components/map/GoogleMap';
-import { useMemo } from 'react';
 
 const MapSectionComponent = () => {
-  const mapOptions = useMemo<google.maps.MapOptions>(
-    () => ({
-      disableDefaultUI: true,
-      clickableIcons: true,
-      scrollwheel: false,
-    }),
-    []
-  );
-
   return (
     <div style={{ width: '2000px', height: '2000px' }}>
-      <GoogleMap mapOptions={mapOptions} />
+      <GoogleMap />
       {/*<KakaoMap />*/}
     </div>
   );
--- src/components/map/GoogleMap.tsx
+++ src/components/map/GoogleMap.tsx
@@ -1,39 +1,70 @@
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
-import { useMemo, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/app/store';
+// import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+// import { useMemo, useRef, useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import { RootState } from '@/app/store';
+//
+// const MapComponent = () => {
+//   const [map, setMap] = useState(null);
+//   const mapRef = useRef(null);
+//
+//   const { lat, lng } = useSelector((state: RootState) => state.map);
+//
+//   const mapOptions = useMemo<google.maps.MapOptions>(
+//     () => ({
+//       disableDefaultUI: true,
+//       clickableIcons: true,
+//       scrollwheel: false,
+//     }),
+//     []
+//   );
+//
+//   const mapCenter = useMemo(() => ({ lat, lng }), [lat, lng]);
+//   const libraries = useMemo(() => ['places'], []);
+//   // const dispatch = useDispatch();
+//   // const { googleMapLoaded } = useSelector((state: RootState) => state.map);
+//
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+//     libraries: libraries as any,
+//   });
+//
+//   return isLoaded ? (
+//     <div ref={mapRef} id="map" style={{ width: '100%' }}>
+//       <GoogleMap
+//         ref={mapRef}
+//         options={mapOptions}
+//         zoom={14}
+//         center={mapCenter}
+//         mapTypeId={google.maps.MapTypeId.ROADMAP}
+//         mapContainerStyle={{ width: '100%', height: '800px' }}
+//         onLoad={() => console.log('Map Component Loaded...')}
+//       />
+//     </div>
+//   ) : (
+//     <p>Loading...</p>
+//   );
+// };
 
-const MapComponent = ({ mapOptions }) => {
-  const [map, setMap] = useState(null);
-  const mapRef = useRef(null);
+import { useEffect } from 'react';
 
-  const { lat, lng } = useSelector((state: RootState) => state.map);
+const MapComponent = () => {
+  useEffect(() => {
+    const initMap = () => {
+      const map = new window.google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          zoom: 8,
+          center: { lat: -34.397, lng: 150.644 },
+        }
+      );
 
-  const mapCenter = useMemo(() => ({ lat, lng }), [lat, lng]);
-  const libraries = useMemo(() => ['places'], []);
-  // const dispatch = useDispatch();
-  // const { googleMapLoaded } = useSelector((state: RootState) => state.map);
+      if (typeof window.google !== 'undefined') {
+        initMap();
+      }
+    };
+  }, []);
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-    libraries: libraries as any,
-  });
-
-  return isLoaded ? (
-    <div ref={mapRef} id="map" style={{ width: '100%' }}>
-      <GoogleMap
-        ref={mapRef}
-        options={mapOptions}
-        zoom={14}
-        center={mapCenter}
-        mapTypeId={google.maps.MapTypeId.ROADMAP}
-        mapContainerStyle={{ width: '100%', height: '800px' }}
-        onLoad={() => console.log('Map Component Loaded...')}
-      />
-    </div>
-  ) : (
-    <p>Loading...</p>
-  );
+  return <div id="map" style={{ height: '1000px', width: '1000px' }}></div>;
 };
 
 export default MapComponent;
